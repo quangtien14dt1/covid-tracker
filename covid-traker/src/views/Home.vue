de03c5d0e2d30aac271d9b6ed623c91fabd96faa
quangtien14dt1 <whynotme1211@gmail.com>
2022-04-19 23:40:16 +0700

    commit1

---
+++ covid-traker/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+<script>
+export default {
+    setup() {
+        const loading = ref(true)
+        const title = ref('Global')
+        const dataDate = ref('')
+        const status = ref('{}')
+        const countries = ref([])
+
+        const fetchCovidData = async () => {
+            const res = await fetch('https://api.covid19api.com/summary')
+            return await res.json()
+        }
+
+        const getCountryData = (country) => {
+            status.value = country
+            title.value = country.Country
+        }
+
+
+    },
+}
+</script>
